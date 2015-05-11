025ce5f9dd6146e6d41ea5c4e0fe11cdc7465845
Forrest G. Choi <chgm1006@gmail.com>
2015-05-11 16:32:54 +0900

    NonSwitchGrade Test

---
+++ src/main/java/junitstudy/testing/NonSwitchGrade.java
@@ -0,0 +1,25 @@
+package junitstudy.testing;
+
+import java.util.Scanner;
+
+/**
+ * Created by Forrest G. Choi on 2015-05-11.
+ */
+public class NonSwitchGrade {
+    public static void main(String[] args) throws Exception{
+        NonSwitchGrade nonSwitchGrade= new NonSwitchGrade();
+        System.out.print("점수는? ");
+        int num = new Scanner(System.in).nextInt();
+        nonSwitchGrade.printGrade(num);
+    }
+
+    public void printGrade(int score) {
+
+        char[] result = {'F', 'F', 'F', 'F', 'F', 'E', 'D', 'C', 'B', 'A', 'A'};
+//        for (int i = 0; i < result.length; i++) {
+//            System.out.println("[" + i + "] = " + "[" + result[i] + "]");
+//        }
+        char ch = result[score / (result.length)];
+        System.out.println(ch);
+    }
+}
